132fd586d11bed073380a79408b919e35ba1221f
denisolek <denis.olek@gmail.com>
2019-06-20 21:59:22 +0200

    fleet repository abstraction refatored

---
+++ fleet/src/main/kotlin/com/denisolek/fleet/cars/infrastructure/CarEntityRepository.kt
@@ -0,0 +1,8 @@
+package com.denisolek.fleet.cars.infrastructure
+
+import org.springframework.data.jpa.repository.JpaRepository
+import java.util.*
+
+interface CarEntityRepository : JpaRepository<CarEntity, UUID> {
+    fun countByRegistrationNumber(registrationNumber: String): Int
+}

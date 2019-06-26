dd8e59507a8f44edcc8f284e634bbb181f1a1270
denisolek <denis.olek@gmail.com>
2019-06-26 15:52:05 +0200

    kubernetes configs v1

---
+++ kubernetes/reset-microk8s.sh
@@ -0,0 +1,16 @@
+rm ngrok.log
+kill $(ps aux | grep 'ngrok' | awk '{print $2}')
+microk8s.reset
+sleep 5
+microk8s.enable dns ingress dashboard
+microk8s.kubectl apply -f kafka-cluster/00-namespace/
+microk8s.kubectl apply -f kafka-cluster/01-zookeeper/
+microk8s.kubectl apply -f kafka-cluster/02-kafka/
+microk8s.kubectl apply -f kafka-cluster/03-yahoo-kafka-manager/
+microk8s.kubectl apply -f rentar-deployment/
+token=$(microk8s.kubectl -n kube-system get secret | grep default-token | cut -d " " -f1)
+microk8s.kubectl -n kube-system describe secret $token
+./ngrok http https://$(microk8s.kubectl get all --all-namespaces |grep kubernetes-dashboard | grep ClusterIP | awk '{print $4}'):443 --log=stdout > ngrok.log &
+sleep 5
+cat ngrok.log | grep 'url=https://' | awk '{print $8}'
+

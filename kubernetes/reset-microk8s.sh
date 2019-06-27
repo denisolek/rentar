edf57f235446bc039623c3d53e29ccef04b35170
root <root@vps617691.ovh.net>
2019-06-27 16:00:44 +0200

    remote

--- kubernetes/reset-microk8s.sh
+++ kubernetes/reset-microk8s.sh
@@ -2,8 +2,10 @@ rm ngrok.log
 kill $(ps aux | grep 'ngrok' | awk '{print $2}')
 microk8s.reset
 sleep 5
-microk8s.enable dns ingress dashboard
-# microk8s.kubectl apply -f kafka-cluster/00-namespace/
+microk8s.enable dns ingress
+microk8s.kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/master/aio/deploy/recommended/kubernetes-dashboard.yaml
+microk8s.kubectl apply -f monitoring/prometheus/
+microk8s.kubectl apply -f monitoring/grafana/
 microk8s.kubectl apply -f kafka-cluster/01-zookeeper/
 microk8s.kubectl apply -f kafka-cluster/02-kafka/
 microk8s.kubectl apply -f kafka-cluster/03-yahoo-kafka-manager/
@@ -13,4 +15,5 @@ microk8s.kubectl -n kube-system describe secret $token
 ./ngrok http https://$(microk8s.kubectl get all --all-namespaces |grep kubernetes-dashboard | grep ClusterIP | awk '{print $4}'):443 --log=stdout > ngrok.log &
 sleep 5
 cat ngrok.log | grep 'url=https://' | awk '{print $8}'
+
 

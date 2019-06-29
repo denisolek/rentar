7b7a5a917b938f284e5659201dc9af4c6e180473
denisolek <denis.olek@gmail.com>
2019-06-29 02:59:00 +0200

    CORS config

--- fleet/build.sh
+++ fleet/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 ./gradlew clean bootJar
-docker rmi denisolek/rentar-fleet:1.0.5
-docker build -t denisolek/rentar-fleet:1.0.5 .
+docker rmi denisolek/rentar-fleet:1.0.6
+docker build -t denisolek/rentar-fleet:1.0.6 .
--- management/publish.sh
+++ management/publish.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 ./gradlew bootJar
-docker rmi denisolek/rentar-management:1.0.1
-docker build -t denisolek/rentar-management:1.0.1 .
-docker push denisolek/rentar-management:1.0.1
+docker rmi denisolek/rentar-management:1.0.2
+docker build -t denisolek/rentar-management:1.0.2 .
+docker push denisolek/rentar-management:1.0.2

0279e30ca061f42f4e647d1d2e1186e32c6e38a9
deniso95 <denis.olek@gmail.com>
2019-06-29 12:58:14 +0200

    added PL locale lang file

--- rentar-admin/src/app.js
+++ rentar-admin/src/app.js
@@ -2,6 +2,7 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import Vue from 'vue';
 import App from 'components/App';
+import pl from './lang/pl-PL.js';
 
 import heyuiConfig from 'js/config/heyui-config';
 import routerConfig from 'js/config/router-config';
@@ -11,17 +12,12 @@ import 'js/vue/filters';
 
 require('./css/app.less');
 
-// 开发环境判断
 // process.env.NODE_ENV == 'development'
 
-// 使用mock文件， 自己开发的时候请删除
 require('./mock/index');
 
-// HeyUI已经设定为全局变量，无需引用
-// 设定全局变量请参考根目录下的hey.conf.js文件
-
 heyuiConfig();
-Vue.use(HeyUI);
+Vue.use(HeyUI, {locale: pl});
 
 const router = routerConfig();
 

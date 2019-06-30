71de7ce4d1304340df71c329258d6b87a35495cc
denisolek <denis.olek@gmail.com>
2019-06-30 13:17:17 +0200

    [WEB] cars view (fetching data from api)

--- rentar-admin/src/components/rentar-components/views/cars.vue
+++ rentar-admin/src/components/rentar-components/views/cars.vue
@@ -1,54 +1,106 @@
 <template>
-  <div>
-    <p>
-      <button class="h-btn h-btn-s h-btn-blue" @click="add(datas)"><i class="h-icon-plus"></i><span>Add a line</span></button>
-    </p>
-    <Table :datas="datas" :height="400">
-      <TableItem title="name" prop="name" :width="150"></TableItem>
-      <TableItem title="age" prop="age" :width="150"></TableItem>
-      <TableItem title="address" prop="address" align="center" :width="150"></TableItem>
-      <TableItem title="name" prop="name" :width="150"></TableItem>
-      <TableItem title="age" prop="age" :width="150"></TableItem>
-      <TableItem title="address" prop="address" align="center" :width="150"></TableItem>
-      <TableItem title="Name" prop="name" :width="150"></TableItem>
-      <TableItem title="age" prop="age" :width="150"></TableItem>
-      <TableItem title="address" prop="address" align="center" :width="150"></TableItem>
-      <TableItem title="operating" :width="100" fixed="right"><template slot-scope="{data}"><button class="h-btn h-btn-s h-btn-red" @click="remove(datas, data)"><i class="h-icon-trash"></i></button></template></TableItem>
-      <div slot="empty">Custom reminder: no data at this time</div>
-    </Table>
+  <div class="h-panel frame-page">
+    <div class="h-panel-bar">
+      <Breadcrumb/>
+    </div>
+    <div class="h-panel-body">
+      <Row>
+        <Cell width="14">
+          <template>
+            <div>
+              <p>
+                <Search @search="carSearch" v-model="carSearchField" placeholder="Szukaj auta"></Search>
+              </p>
+            </div>
+          </template>
+          <div>
+            <div>
+              <Table :datas="carTable" :height="400">
+                <TableItem title="Nr rejestracyjny" prop="registrationNumber" :width="150"></TableItem>
+                <TableItem title="Marka" prop="manufacturer" :width="150"></TableItem>
+                <TableItem title="Model" prop="model" :width="150"></TableItem>
+                <TableItem title="Rocznik" prop="productionYear" :width="150"></TableItem>
+                <TableItem title="Akcja" align="center" :width="100">
+                  <template slot-scope="{data}">
+                    <router-link :to="{name: 'Car', params: {id: data.id}}">
+                      <button class="h-btn h-btn-blue h-btn-circle">
+                        <i class="h-icon-edit"></i>
+                      </button>
+                    </router-link>
+                  </template>
+                </TableItem>
+                <div slot="empty">Brak danych</div>
+              </Table>
+            </div>
+            <Loading text="Loading" :loading="loadingCars"></Loading>
+          </div>
+        </Cell>
+        <Cell width="10">
+          <AddCar/>
+        </Cell>
+      </Row>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      datas: [
-        { id: 5, name: 'test 5', age: 12, address: 'Shanghai' },
-        { id: 6, name: 'test 6', age: 12, address: 'Shanghai' },
-        { id: 7, name: 'test 7', age: 12, address: 'Shanghai' },
-        { id: 5, name: 'test 5', age: 12, address: 'Shanghai' },
-        { id: 6, name: 'test 6', age: 12, address: 'Shanghai' },
-        { id: 7, name: 'test 7', age: 12, address: 'Shanghai' },
-        { id: 7, name: 'test 7', age: 12, address: 'Shanghai' },
-        { id: 5, name: 'test 5', age: 12, address: 'Shanghai' },
-        { id: 6, name: 'test 6', age: 12, address: 'Shanghai' },
-        { id: 7, name: 'test 7', age: 12, address: 'Shanghai' },
-        { id: 7, name: 'test 7', age: 12, address: 'Shanghai' },
-        { id: 7, name: 'test 7', age: 12, address: 'Shanghai' },
-        { id: 5, name: 'test 5', age: 12, address: 'Shanghai' },
-        { id: 6, name: 'test 6', age: 12, address: 'Shanghai' },
-        { id: 7, name: 'test 7', age: 12, address: 'Shanghai' }
-      ]
-    };
-  },
-  methods: {
-    remove(datas, data) {
-      datas.splice(datas.indexOf(data), 1);
+  import AddCar from 'components/rentar-components/addCar'
+  import Breadcrumb from 'components/rentar-components/breadcrumbs/carsBC'
+
+  export default {
+    data() {
+      return {
+        loadingCars: false,
+        carSearchField: null,
+        carTable: [],
+        cars: [],
+      }
     },
-    add(datas) {
-      datas.push({ id: 7, name: 'Add to', age: 12, address: 'Then added' });
-    }
+    created() {
+      this.fetchCars();
+      this.$bus.on('CarCreated', () => {
+        setTimeout(() => {
+          this.fetchCars()
+        }, 700);
+      })
+    },
+    methods: {
+      mapToTable: function (cars) {
+        return cars.map(car => ({
+          id: car.id,
+          registrationNumber: car.registrationNumber,
+          manufacturer: car.manufacturer,
+          model: car.model,
+          productionYear: car.productionYear
+        }));
+      }, fetchCars() {
+        this.loadingCars = true;
+        R.Cars.fetchAll().then(resp => {
+          if (resp.status === 200) {
+            this.loadingCars = false;
+            this.cars = resp.data;
+            this.carTable = this.mapToTable(this.cars)
+          }
+        })
+      },
+      carSearch(value) {
+        if (value === '') {
+          this.carTable = this.mapToTable(this.cars)
+        } else {
+          let loweredValue = value.toLowerCase();
+          let filteredCars = this.cars.filter(function (el) {
+            return el.registrationNumber.toLowerCase().includes(loweredValue) ||
+              el.manufacturer.toLowerCase().includes(loweredValue) ||
+              el.model.toLowerCase().includes(loweredValue) ||
+              el.productionYear.toString().includes(loweredValue)
+          });
+          this.carTable = this.mapToTable(filteredCars)
+        }
+      },
+    },
+    components: {
+      Breadcrumb,
+      AddCar,
+    },
   }
-};
 </script>

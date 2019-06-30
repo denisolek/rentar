93cf7db4f2c84c76ab2e7daef84529080b56543d
denisolek <denis.olek@gmail.com>
2019-06-30 15:14:03 +0200

    [Populator] - generating stuff async (multi-threading)

--- populator/src/main/kotlin/App.kt
+++ populator/src/main/kotlin/App.kt
@@ -1,21 +1,41 @@
 import fleet.FleetFacade
 import management.ManagementFacade
 import rental.RentalFacade
+import java.util.*
+import kotlin.concurrent.schedule
 
 object Setup {
     const val apiHost = "https://api.rentar.eu"
     const val requestInterval = 0
-    const val customersCount = 0
-    const val carCount = 0
-    const val rentalsCount = 100
+    const val customersCount = 200
+    const val carCount = 200
+    const val rentalsCount = 200
 }
 
 fun main() {
-    val createdCustomers = ManagementFacade().addCustomers(Setup.customersCount)
-    ManagementFacade().updateCustomers(createdCustomers)
-    val cars = FleetFacade().addCars(Setup.carCount)
-    FleetFacade().updateCars(cars)
-    val allCustomers = ManagementFacade().getAll()
-    val rentals = RentalFacade().createRentals(Setup.rentalsCount, allCustomers)
-    RentalFacade().cancelRentals(rentals)
+    generateCustomers()
+    generateCars()
+    generateRentals()
+}
+
+private fun generateCustomers() {
+    Thread {
+        val createdCustomers = ManagementFacade().addCustomers(Setup.customersCount)
+        ManagementFacade().updateCustomers(createdCustomers)
+    }.start()
+}
+
+private fun generateCars() {
+    Thread {
+        val cars = FleetFacade().addCars(Setup.carCount)
+        FleetFacade().updateCars(cars)
+    }.start()
+}
+
+private fun generateRentals() {
+    Timer().schedule(10000) {
+        val allCustomers = ManagementFacade().getAll()
+        val rentals = RentalFacade().createRentals(Setup.rentalsCount, allCustomers)
+        RentalFacade().cancelRentals(rentals)
+    }
 }

06449048736e8d12aabc89ebc13a87525b67a6fd
denisolek <denis.olek@gmail.com>
2019-06-30 03:06:53 +0200

    [Management] - added email field to DetailedUser - fixed updateCustomer validator (considering current customer)

--- management/src/main/kotlin/com/denisolek/management/customers/facade/CustomerQueryHandler.kt
+++ management/src/main/kotlin/com/denisolek/management/customers/facade/CustomerQueryHandler.kt
@@ -6,6 +6,10 @@ import com.denisolek.management.customers.facade.query.DetailedCustomer
 import com.denisolek.management.customers.facade.query.UpdateCustomerValidate
 import com.denisolek.management.customers.infrastructure.CustomerExceptions.*
 import com.denisolek.management.customers.infrastructure.config.CustomerRepository
+import com.denisolek.management.customers.model.value.CustomerId
+import com.denisolek.management.customers.model.value.DrivingLicence
+import com.denisolek.management.customers.model.value.Email
+import com.denisolek.management.customers.model.value.Passport
 import org.springframework.stereotype.Service
 import java.util.*
 
@@ -13,20 +17,22 @@ import java.util.*
 class CustomerQueryHandler(val repository: CustomerRepository) {
     fun validateAdd(dto: AddCustomerValidate) {
         when {
-            repository.countByEmail(dto.email) > 0 -> throw EmailAlreadyExistsException()
-            repository.countByDrivingLicence(dto.drivingLicence) > 0 -> throw DrivingLicenceAlreadyExistsException()
-            repository.countByPassport(dto.passport) > 0 -> throw PassportAlreadyExistsException()
+            repository.findByEmail(Email(dto.email)).isNotEmpty() ->
+                throw EmailAlreadyExistsException()
+            repository.findByDrivingLicence(DrivingLicence(dto.drivingLicence)).isNotEmpty() ->
+                throw DrivingLicenceAlreadyExistsException()
+            repository.findByPassport(Passport(dto.passport)).isNotEmpty() ->
+                throw PassportAlreadyExistsException()
         }
     }
 
     fun validateUpdate(dto: UpdateCustomerValidate, id: UUID) {
-        val customer = repository.findByIdOrThrow(id)
         when {
-            customer.email.equals(dto.email) && repository.countByEmail(dto.email) > 0 ->
+            repository.findByEmail(Email(dto.email)).firstOrNull()?.id != CustomerId(id) ->
                 throw EmailAlreadyExistsException()
-            customer.drivingLicence.equals(dto.drivingLicence) && repository.countByDrivingLicence(dto.drivingLicence) > 0 ->
+            repository.findByDrivingLicence(DrivingLicence(dto.drivingLicence)).firstOrNull()?.id != CustomerId(id) ->
                 throw DrivingLicenceAlreadyExistsException()
-            customer.passport.equals(dto.passport) && repository.countByPassport(dto.passport) > 0 ->
+            repository.findByPassport(Passport(dto.passport)).firstOrNull()?.id != CustomerId(id) ->
                 throw PassportAlreadyExistsException()
         }
     }
--- management/src/main/kotlin/com/denisolek/management/customers/infrastructure/config/CustomerRepository.kt
+++ management/src/main/kotlin/com/denisolek/management/customers/infrastructure/config/CustomerRepository.kt
@@ -4,30 +4,33 @@ import com.denisolek.management.customers.infrastructure.CustomerEntity
 import com.denisolek.management.customers.infrastructure.CustomerEntityRepository
 import com.denisolek.management.customers.infrastructure.CustomerExceptions
 import com.denisolek.management.customers.model.Customer
+import com.denisolek.management.customers.model.value.DrivingLicence
+import com.denisolek.management.customers.model.value.Email
+import com.denisolek.management.customers.model.value.Passport
 import com.denisolek.management.infrastructure.findOne
 import org.springframework.stereotype.Component
 import java.util.*
 
 @Component
 class CustomerRepository(private val repository: CustomerEntityRepository) {
-    fun countByEmail(email: String): Int {
-        return repository.countByEmail(email)
-    }
-
-    fun countByDrivingLicence(drivingLicence: String): Int {
-        return repository.countByDrivingLicence(drivingLicence)
-    }
-
-    fun countByPassport(passport: String): Int {
-        return repository.countByPassport(passport)
-    }
-
     fun save(customer: Customer): Customer {
         return repository.save(CustomerEntity(customer)).toDomainModel()
     }
 
     fun findByIdOrThrow(id: UUID): Customer {
         return repository.findOne(id)?.toDomainModel() ?: throw CustomerExceptions.CustomerNotFoundException()
+    }
+
+    fun findByEmail(email: Email): List<Customer> {
+        return repository.findByEmail(email.value).map { it.toDomainModel() }
+    }
+
+    fun findByDrivingLicence(drivingLicence: DrivingLicence): List<Customer> {
+        return repository.findByDrivingLicence(drivingLicence.value).map { it.toDomainModel() }
+    }
+
+    fun findByPassport(passport: Passport): List<Customer> {
+        return repository.findByPassport(passport.value).map { it.toDomainModel() }
     }
 
     fun findAll(): List<Customer> {

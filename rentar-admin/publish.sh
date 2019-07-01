69dff3a9c9b5c8c69ea6eb5e684a93c0ecf9d52f
denisolek <denis.olek@gmail.com>
2019-07-01 06:03:48 +0200

    [Web] 1.0.1 nginx config should fix vue-router (404 problem after refreshing)

--- rentar-admin/publish.sh
+++ rentar-admin/publish.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 npm install
 hey build
-docker rmi denisolek/rentar-web:1.0.0
-docker build -t denisolek/rentar-web:1.0.0 .
-docker push denisolek/rentar-web:1.0.0
+docker rmi denisolek/rentar-web:1.0.1
+docker build -t denisolek/rentar-web:1.0.1 .
+docker push denisolek/rentar-web:1.0.1

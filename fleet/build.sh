de27fb8d4edba9e1b3db07783c672329a2aa7562
denisolek <denis.olek@gmail.com>
2019-07-01 03:27:38 +0200

    [Fleet] 1.0.8 validating update should be fixed

--- fleet/build.sh
+++ fleet/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 ./gradlew clean bootJar
-docker rmi denisolek/rentar-fleet:1.0.7
-docker build -t denisolek/rentar-fleet:1.0.7 .
+docker rmi denisolek/rentar-fleet:1.0.8
+docker build -t denisolek/rentar-fleet:1.0.8 .
